549c0bec0ca5a77555139b8fece6dce0ea97003b
israelcoder <israelgcjoaquim@gmail.com>
2024-10-10 20:23:11 -0300

    20:23 commit

--- pages/render/clock.jsx
+++ pages/render/clock.jsx
@@ -4,7 +4,6 @@ export default function clock(){
     return (
         <div class="m-auto flex justify-center items-center h-screen bg-[#141414]">
             <div class=" m-auto flex justify-center text-center w-[90%] h-[100%] overflow-x-hidden overflow-y-hidden">
-                <h1>opa</h1>
                 <Clock/>
             </div>
         </div>
