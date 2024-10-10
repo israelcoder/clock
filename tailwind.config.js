c56f4b2d2dde35911bb5cc0a39dace39ded5c7af
israelcoder <israelgcjoaquim@gmail.com>
2024-10-10 15:01:00 -0300

    restaurando

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily:{
+      'serrat':['Montserrat', 'sans-serif'],
+      'Oswald':['Oswald','serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
